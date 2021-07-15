3f6e13c668ff24b7e2564dd3fd60fd7baa1b48da
Usvaandr <andrius.usvaltas@gmail.com>
2021-07-15 15:50:22 +0300

    Updated QueryBuilder file with better variable naming.

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -2,6 +2,9 @@
 
 class QueryBuilder
 {
+    /**
+     * @var PDO
+     */
     protected $pdo;
 
     public function __construct(PDO $pdo)
@@ -9,17 +12,17 @@ class QueryBuilder
         $this->pdo = $pdo;
     }
 
-    public function selectRow(string $table, int $id): object
+    public function selectRow(string $tableName, int $id): object
     {
-        $statement = $this->pdo->prepare("SELECT * FROM {$table} WHERE id = {$id}");
+        $statement = $this->pdo->prepare("SELECT * FROM {$tableName} WHERE id = {$id}");
         $statement->execute();
 
         return $statement->fetchAll(PDO::FETCH_CLASS)[0];
     }
 
-    public function selectName(string $table, int $id): string
+    public function selectName(string $tableName, int $id): string
     {
-        $statement = $this->pdo->prepare("SELECT name FROM {$table} WHERE id = {$id};");
+        $statement = $this->pdo->prepare("SELECT name FROM {$tableName} WHERE id = {$id};");
         $statement->execute();
 
         return $statement->fetchAll(PDO::FETCH_OBJ)[0]->name;
